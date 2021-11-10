8db86593e42fe981f3037a151cfbc210ec1fe21c
Diogozura <65250673+Diogozura@users.noreply.github.com>
2021-11-09 22:12:29 -0300

    teste do site com forms de musica

---
+++ forms Produção/script.js
@@ -0,0 +1,7 @@
+// alert("Esse site é apenas um exemplo")
+
+const titulo = document.querySelector("#titulo")
+const descricao = document.querySelector("#descricao");
+
+titulo.textContent = "Músicas para o intervalo"
+descricao.textContent = "mandem músicas para tocarmos no intervalo de 1h"
